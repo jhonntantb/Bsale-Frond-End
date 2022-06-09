fb58ae6959b507f85815085c1e70ec9d65468627
jhonntantb <jhonntantb@gmail.com>
2022-06-09 13:38:20 -0500

    click logo query all products

--- src/js/listProducts.js
+++ src/js/listProducts.js
@@ -5,8 +5,17 @@ import { DisplayListProducts, SetupPagination} from './pagination.js';
 const list_products = document.getElementById("listProducts");
 const pagination_element = document.getElementById("pagination");
 
+/* charge web*/
 DisplayListProducts(1, null, list_products, getProducts);
 SetupPagination(null, pagination_element,getProducts);
 
+/* push click Logo */
+
+const logo_element = document.getElementById("logo");
+
+logo_element.addEventListener("click",() => {
+    DisplayListProducts(1, null, list_products, getProducts);
+    SetupPagination(null, pagination_element,getProducts);
+})
 
 export default list_products;
